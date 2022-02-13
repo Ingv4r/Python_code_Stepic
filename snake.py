d4a1fdfe717b69b8181b0f135c4981da4e01a9e7
Ingv4r <87081544+Ingv4r@users.noreply.github.com>
2022-02-13 14:38:19 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -2,7 +2,6 @@ import pygame
 import time 
 import random 
 
- 
 pygame.init() 
  
 dis_width = 800
@@ -23,60 +22,79 @@ clock = pygame.time.Clock()
 font_style = pygame.font.SysFont('bahnschrift', 30)
 score_font = pygame.font.SysFont('comicsansms', 35) 
 
+snake_speed = 180
+food_radius = 8
+
+def game_vars():
+    vars_dict = {
+                'x1' : dis_width / 2,
+                'y1' : dis_height / 2,
+                'x1_change' : 0, 
+                'y1_change' : 0, 
+                'snake_list' : [],
+                'length_of_snake' : 1,
+                'foodx' : round(random.randrange(food_radius, dis_width - food_radius)), 
+                'foody' : round(random.randrange(food_radius, dis_height - food_radius)), 
+                'game' : True,
+                'endgame' : False, 
+    }
+    return vars_dict
+
+
 def quit_game():
+    pygame.quit()
     quit()
 
 def endgame_keypress(event):
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_q:
+    if event.key == pygame.K_q:
+        quit_game()
+    elif event.key == pygame.K_p:  
+        dis.fill(black)
+        return game_vars()
+
+def endgame_dis(score):
+    dis.fill(black) 
+    height_of_score = 3
+    height_of_message = 2
+    width_of_score = 2
+    width_of_message = 2
+    render_text_and_position("Your score: " + str(score), score_font, white, width_of_score, height_of_score)
+    render_text_and_position("Press 'Q' to Quit or 'P' to Play Again", font_style, red, width_of_message, height_of_message)
+             
+    pygame.display.update()        
+         
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
             quit_game()
-        elif event.key == pygame.K_p: 
-            dis.fill(black) 
-            gameloop()
-
-def move_keypress(event):
-    x1_change = 0
-    y1_change = 0
-    pixel = 1
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_w:
-            x1_change = 0
-            y1_change = -pixel
-            return x1_change, y1_change
-        elif event.key == pygame.K_a:
-            x1_change = -pixel
-            y1_change = 0
-            return x1_change, y1_change
-        elif event.key == pygame.K_s:
-            x1_change = 0
-            y1_change = pixel
-            return x1_change, y1_change
-        elif event.key == pygame.K_d:
-            x1_change = pixel
-            y1_change = 0
-            return x1_change, y1_change
+        elif event.type == pygame.KEYDOWN:
+            endgame_keypress(event)
 
 
-def draw_snake(snake_list, color = white, circle_rad = 10):
-    for element in snake_list:
-        pygame.draw.circle(dis, color, (element[0], element[1]), circle_rad)
-
-
-def render_text_and_position(text, font, color, width = 0, height = 0): 
-    message = font.render(text, True, color)
-    position = [0, 0]
-    if width != 0 and height != 0:
-        position = message.get_rect(center = (dis_width / width, dis_height / height))
-    dis.blit(message, position)
-
+def move_keypress(event, snake_list):
+    stepSize = 1
+    if event.key == pygame.K_w:
+        if check_collision(snake_list, 'w'):
+            deltaX = 0
+            deltaY = -stepSize
+            return deltaX, deltaY
+    elif event.key == pygame.K_a:
+        if check_collision(snake_list, 'a'):
+            deltaX = -stepSize
+            deltaY = 0
+            return deltaX, deltaY
+    elif event.key == pygame.K_s:
+        if check_collision(snake_list, 's'):
+            deltaX = 0
+            deltaY = stepSize
+            return deltaX, deltaY
+    elif event.key == pygame.K_d:
+        if check_collision(snake_list, 'd'):
+            deltaX = stepSize
+            deltaY = 0
+            return deltaX, deltaY
 
 def check_collision(snake_list, key): # -> bool
-    if True:
-        pass
-    
-    
-    
-'''if key == 'a'
+    if key == 'a':
         if len(snake_list) == 1 or snake_list[-1][0] != (snake_list[-2][0] + 1): 
             return True 
         else:
@@ -95,91 +113,77 @@ def check_collision(snake_list, key): # -> bool
         if len(snake_list) == 1 or snake_list[-1][0] != (snake_list[-2][0] - 1):
             return True
         else:
-            return False'''
+            return False
 
+def draw_snake(snake_list, color = white, circle_rad = 10):
+    for element in snake_list:
+        pygame.draw.circle(dis, color, (element[0], element[1]), circle_rad)
+
+
+def render_text_and_position(text, font, color, width = 0, height = 0): 
+    message = font.render(text, True, color)
+    position = [0, 0]
+    if width != 0 and height != 0:
+        position = message.get_rect(center = (dis_width / width, dis_height / height))
+    dis.blit(message, position)
+    
 
 def gameloop(): 
-    snake_speed = 180
- 
-    x1 = dis_width / 2 
-    y1 = dis_height / 2 
- 
-    x1_change = 0 
-    y1_change = 0 
+    
+    vars = game_vars()
 
-    snake_list = []
-    length_of_snake = 1
-
-    food_radius = 8
-    foodx = round(random.randrange(food_radius, dis_width - food_radius)) 
-    foody = round(random.randrange(food_radius, dis_height - food_radius)) 
+    while vars['game']: 
  
-    game = True
-    game_close = False
-
-    while game: 
- 
-        while game_close == True: 
-            dis.fill(black) 
-            height_of_score = 3
-            height_of_message = 2
-            width_of_score = 2
-            width_of_message = 2
-            render_text_and_position("Your score: " + str(score), score_font, white, width_of_score, height_of_score)
-            render_text_and_position("Press 'Q' to Quit or 'P' to Play Again", font_style, red, width_of_message, height_of_message)
-             
-            pygame.display.update()        
-         
-            for event in pygame.event.get(): 
-                if event.type == pygame.QUIT:
-                    quit_game()
-                else:
-                    endgame_keypress(event)
+        while vars['endgame'] == True: 
+            vars_new = endgame_dis(score)
+            if vars_new != None:
+                vars = vars_new
 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 quit_game()
-            keypress = move_keypress(event)
-            if keypress != None:
-                x1_change = keypress[0]
-                y1_change = keypress[1]
+            elif event.type == pygame.KEYDOWN:
+                keypress = move_keypress(event, vars['snake_list'])
+                if keypress != None:
+                    vars['x1_change'] = keypress[0]
+                    vars['y1_change'] = keypress[1]
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0: 
-            game_close = True  
+        if vars['x1'] >= dis_width or vars['x1'] < 0 or vars['y1'] >= dis_height or vars['y1'] < 0: 
+            vars['endgame'] = True  
  
-        x1 += x1_change 
-        y1 += y1_change 
+        vars['x1'] += vars['x1_change'] 
+        vars['y1'] += vars['y1_change'] 
         
         dis.fill(black)
 
-        pygame.draw.circle(dis, green, (foodx, foody), food_radius)
+        pygame.draw.circle(dis, green, (vars['foodx'], vars['foody']), food_radius)
 
         snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
-        snake_list.append(snake_head)
-        if len(snake_list) > length_of_snake:
-            del snake_list[0]
+        snake_head.append(vars['x1'])
+        snake_head.append(vars['y1'])
+        vars['snake_list'].append(snake_head)
+        if len(vars['snake_list']) > vars['length_of_snake']:
+            del vars['snake_list'][0]
 
-        for x in snake_list[:-1:1]:
+        for x in vars['snake_list'][:-1:1]:
             if x == snake_head:
-                game_close = True
+                vars['endgame'] = True
         
-        draw_snake(snake_list)
-        score = length_of_snake // 10
+        draw_snake(vars['snake_list'])
+
+        score = vars['length_of_snake'] // 10
         render_text_and_position('Your score: ' + str(score), score_font, yellow)
  
         pygame.display.update() 
  
-        if (foodx - 11) <= x1 <= (foodx + 11) and (foody - 11) <= y1 <= (foody + 11):
-            foodx = round(random.randrange(food_radius, dis_width - food_radius)) 
-            foody = round(random.randrange(food_radius, dis_height - food_radius)) 
-            length_of_snake += 10
+        if (vars['foodx'] - 11) <= vars['x1'] <= (vars['foodx'] + 11) and (vars['foody'] - 11) <= vars['y1'] <= (vars['foody'] + 11):
+            vars['foodx'] = round(random.randrange(food_radius, dis_width - food_radius)) 
+            vars['foody'] = round(random.randrange(food_radius, dis_height - food_radius)) 
+            vars['length_of_snake'] += 10
         
         clock.tick(snake_speed) 
  
-    pygame.quit() 
-    quit() 
+    quit_game()
  
-
+ 
 gameloop()

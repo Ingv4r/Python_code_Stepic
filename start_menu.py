5a7130c3af27f8f75f8613dc13714aa3a818f57e
Ingv4r <87081544+Ingv4r@users.noreply.github.com>
2022-02-23 14:26:37 +0300

    Update start_menu.py

--- start_menu.py
+++ start_menu.py
@@ -1,16 +1,21 @@
 import pygame as pg
-import snake
+from snake import *
 
 pg.init()
 pg.display.set_caption("Igor's first snake game v1.5")
 
-class SnakeMenu():
-    def __init__(self):
+class Menu_var():
+    def __init__(self) -> None:
         self.text_width = '600'
         self.text_height = '400'
-        self.screen = pg.display.set_mode((int(text), int(text2)))
+        self.screen = pg.display.set_mode((int(self.text_width), int(self.text_height)))
         self.font = pg.font.Font(None, 30)
         self.clock = pg.time.Clock()
+        self.done = False
+
+class buttons_var():
+    def __init__(self) -> None:
+        # buttons position
         self.resolution_width = pg.Rect(330, 120, 58, 30)
         self.resolution_height = pg.Rect(430, 120, 58, 30)
         self.difficulty_box_easy = pg.Rect(300, 180, 55, 30)
@@ -18,135 +23,143 @@ class SnakeMenu():
         self.difficulty_box_hard = pg.Rect(485, 180, 57, 30)
         self.Start_box = pg.Rect(200, 290, 75, 30)
         self.Quit_box = pg.Rect(330, 290, 75, 30)
-        self.color_inactive = pg.Color('lightskyblue3')
-        self.color_active = pg.Color('dodgerblue2')
+        # activation of color
         self.active_width = False
         self.active_height= False
         self.active_easy = False
         self.active_med = False
         self.active_hard = False
-        self.done = False
+        # color of buttons
+        self.color_inactive = pg.Color('lightskyblue3')
+        self.color_active = pg.Color('dodgerblue2')
+        self.color_width = False
+        self.color_height = False
+        self.color_easy = False
+        self.color_med = False
+        self.color_hard = False
 
+mn = Menu_var()
+btn = buttons_var()
 
-    def start_game(self):
-        self.done = True
-        if self.active_easy == True:
-            dificulty = 'easy'
-        elif self.active_med == True:
-            dificulty = 'medium'
-        elif self.active_hard == True:
-            dificulty = 'hard'
-        snake.game_settings(int(self.text_width), int(self.text_height), dificulty)
-        snake.gameloop()
+def start_game():
+    mn.done = True
+    if btn.active_easy == True:
+        dificulty = 'easy'
+    elif btn.active_med == True:
+        dificulty = 'medium'
+    elif btn.active_hard == True:
+        dificulty = 'hard'
+    game_settings(int(mn.text_width), int(mn.text_height), dificulty)
+    gameloop()
 
-    def mouse_click(self, event):
-        if self.resolution_width.collidepoint(event.pos):
-            self.active_width = True
-            self.active_height = False
-        elif self.resolution_height.collidepoint(event.pos):
-            self.active_width = False
-            self.active_height = True
-        elif self.difficulty_box_easy.collidepoint(event.pos):
-            self.active_easy = True
-            self.active_med = False
-            self.active_hard = False
-            self.active_width = False
-            self.active_height = False
-        elif self.difficulty_box_med.collidepoint(event.pos):
-            self.active_easy = False
-            self.active_med = True
-            self.active_hard = False
-            self.active_width = False
-            self.active_height = False
-        elif self.difficulty_box_hard.collidepoint(event.pos):
-            self.active_easy = False
-            self.active_med = False
-            self.active_hard = True
-            self.active_width = False
-            self.active_height = False
-        elif self.Start_box.collidepoint(event.pos):
-            self.start_game()
-        elif self.Quit_box.collidepoint(event.pos):
-            self.done = True
+def mouse_click( event):
+    if btn.resolution_width.collidepoint(event.pos):
+        btn.active_width = True
+        btn.active_height = False
+    elif btn.resolution_height.collidepoint(event.pos):
+        btn.active_width = False
+        btn.active_height = True
+    elif btn.difficulty_box_easy.collidepoint(event.pos):
+        btn.active_easy = True
+        btn.active_med = False
+        btn.active_hard = False
+        btn.active_width = False
+        btn.active_height = False
+    elif btn.difficulty_box_med.collidepoint(event.pos):
+        btn.active_easy = False
+        btn.active_med = True
+        btn.active_hard = False
+        btn.active_width = False
+        btn.active_height = False
+    elif btn.difficulty_box_hard.collidepoint(event.pos):
+        btn.active_easy = False
+        btn.active_med = False
+        btn.active_hard = True
+        btn.active_width = False
+        btn.active_height = False
+    elif btn.Start_box.collidepoint(event.pos):
+        start_game()
+    elif btn.Quit_box.collidepoint(event.pos):
+        mn.done = True
+    else:
+        btn.active_width = False
+        btn.active_height = False
+
+def buttons_text(event):
+    if btn.active_width:
+        if event.key == pg.K_RETURN:
+            mn.text_width = ''    
+        elif event.key == pg.K_BACKSPACE:
+            mn.text_width = mn.text_width[:-1]
         else:
-            self.active_width = False
-            self.active_height = False
+            mn.text_width += event.unicode
+    if btn.active_height:
+        if event.key == pg.K_RETURN:
+            mn.text_height = ''
+        elif event.key == pg.K_BACKSPACE:
+            mn.text_height = mn.text_height[:-1]
+        else:
+            mn.text_height += event.unicode
 
+def buttons_text():
+    txt_width = mn.font.render(mn.text_width, True, btn.color_width)
+    txt_height = mn.font.render(mn.text_height, True, btn.color_height)
+    txt_easy = mn.font.render('easy', True, btn.color_inactive)
+    txt_med = mn.font.render('medium', True, btn.color_inactive)
+    txt_hard = mn.font.render('hard', True, btn.color_inactive)
+    txt_Start = mn.font.render('START', True, btn.color_inactive)
+    txt_Quit = mn.font.render(' QUIT', True, btn.color_inactive)
+    # Blit the text.
+    mn.screen.blit(txt_width, (btn.resolution_width.x+5, btn.resolution_width.y+5))
+    mn.screen.blit(txt_height, (btn.resolution_height.x+5, btn.resolution_height.y+5))
+    mn.screen.blit(txt_easy, (btn.difficulty_box_easy.x+5, btn.difficulty_box_easy.y+5))
+    mn.screen.blit(txt_med, (btn.difficulty_box_med.x+5, btn.difficulty_box_med.y+5))
+    mn.screen.blit(txt_hard, (btn.difficulty_box_hard.x+5, btn.difficulty_box_hard.y+5))
+    mn.screen.blit(txt_Start, (btn.Start_box.x+5, btn.Start_box.y+5))
+    mn.screen.blit(txt_Quit, (btn.Quit_box.x+5, btn.Quit_box.y+5))
 
-    def menu(self):
-        while not self.done:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.done = True
-                if event.type == pg.MOUSEBUTTONDOWN:
-                    # If the user clicked on the input_box rect.
-                    self.mouse_click(event)
-                  
-                        
-                    # Change the current color of the input box.
-                    color = color_active if active else color_inactive
-                    color2 = color_active if active2 else color_inactive
-                    color3 = color_active if active3 else color_inactive
-                    color4 = color_active if active4 else color_inactive
-                    color5 = color_active if active5 else color_inactive
-        
-                if event.type == pg.KEYDOWN:
-                    if active:
-                        if event.key == pg.K_RETURN:
-                            print(text)
-                            text = ''
-                            
-                        elif event.key == pg.K_BACKSPACE:
-                            text = text[:-1]
-                        else:
-                            text += event.unicode
-                    if active2:
-                        if event.key == pg.K_RETURN:
-                            print(text2)
-                            text2 = ''
-                        elif event.key == pg.K_BACKSPACE:
-                            text2 = text2[:-1]
-                        else:
-                            text2 += event.unicode
+def render_button():
+    pg.draw.rect(mn.screen, btn.color_width, btn.resolution_width, 2)
+    pg.draw.rect(mn.screen, btn.color_height, btn.resolution_height, 2)
+    pg.draw.rect(mn.screen, btn.active_easy, btn.difficulty_box_easy, 2)
+    pg.draw.rect(mn.screen, btn.active_med, btn.difficulty_box_med, 2)
+    pg.draw.rect(mn.screen, btn.active_hard, btn.difficulty_box_hard, 2)
+    pg.draw.rect(mn.screen, btn.color_inactive, btn.Start_box, 2)
+    pg.draw.rect(mn.screen, btn.color_inactive, btn.Quit_box, 2)
+
+def render_info():
+    render_text_and_position('Hello world!', mn.font, (255, 255, 255), mn.screen, 300, 20)
+    render_text_and_position('Choose resolution:', mn.font, (255, 255, 255), mn.screen, 150, 135)
+    render_text_and_position('x', mn.font, btn.color_inactive, mn.screen, 408, 135)
+    render_text_and_position('Choose difficulty:', mn.font, (255, 255, 255), mn.screen, 150, 200)
+    render_text_and_position('Hello world!', mn.font, (255, 255, 255), mn.screen, 300, 20)
+
+def main():
+    while not mn.done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                mn.done = True
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                mouse_click(event)    
+                # Change the current color of the input box.
+                btn.color_width = btn.color_active if btn.active_width else btn.color_inactive
+                btn.color_height = btn.color_active if btn.active_height else btn.color_inactive                
+                btn.color_easy = btn.color_active if btn.active_easy else btn.color_inactive
+                btn.color_med = btn.color_active if btn.active_med else btn.color_inactive
+                btn.color_hard = btn.color_active if btn.active_hard else btn.color_inactive
+            elif event.type == pg.KEYDOWN:
+                buttons_text(event)
                 
+        mn.screen.fill((30, 30, 30))
+        buttons_text()
+        # Blit the input_box rect.
+        render_button()
+        render_info()
 
-            screen.fill((30, 30, 30))
-            # Render the current text.
-            txt_resX = font.render(text, True, color)
-            txt_resY = font.render(text2, True, color2)
-            txt_easy = font.render('easy', True, color_inactive)
-            txt_med = font.render('medium', True, color_inactive)
-            txt_hard = font.render('hard', True, color_inactive)
-            txt_Start = font.render('START', True, color_inactive)
-            txt_Quit = font.render(' QUIT', True, color_inactive)
-            # Blit the text.
-            screen.blit(txt_resX, (res_boxX.x+5, res_boxX.y+5))
-            screen.blit(txt_resY, (res_boxY.x+5, res_boxY.y+5))
-            screen.blit(txt_easy, (diff_box_easy.x+5, diff_box_easy.y+5))
-            screen.blit(txt_med, (diff_box_med.x+5, diff_box_med.y+5))
-            screen.blit(txt_hard, (diff_box_hard.x+5, diff_box_hard.y+5))
-            screen.blit(txt_Start, (Start_box.x+5, Start_box.y+5))
-            screen.blit(txt_Quit, (Quit_box.x+5, Quit_box.y+5))
-            # Blit the input_box rect.
-            pg.draw.rect(screen, color, res_boxX, 2)
-            pg.draw.rect(screen, color2, res_boxY, 2)
-            pg.draw.rect(screen, color3, diff_box_easy, 2)
-            pg.draw.rect(screen, color4, diff_box_med, 2)
-            pg.draw.rect(screen, color5, diff_box_hard, 2)
-            pg.draw.rect(screen, color_inactive, Start_box, 2)
-            pg.draw.rect(screen, color_inactive, Quit_box, 2)
-
-            snake.render_text_and_position('Hello world!', font, (255, 255, 255), screen, 300, 20)
-            snake.render_text_and_position('Choose resolution:', font, (255, 255, 255), screen, 150, 135)
-            snake.render_text_and_position('x', font, color_inactive, screen, 408, 135)
-            snake.render_text_and_position('Choose difficulty:', font, (255, 255, 255), screen, 150, 200)
-            snake.render_text_and_position('Hello world!', font, (255, 255, 255), screen, 300, 20)
-            
-
-            pg.display.flip()
-            clock.tick(30)
+        pg.display.flip()
+        mn.clock.tick(30)
 
 
 if __name__ == '__main__':
-    m = SnakeMenu()
-    m.menu()
+    main()
